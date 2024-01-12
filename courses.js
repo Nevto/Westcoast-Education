fa2df57417398bb223aa4c45862e2cd2319342fa
Jonathan Norberg <Jonathannorberg95@gmail.com>
2024-01-12 17:16:18 +0100

    added html file with basic code, added courses and main.js. added dummy data and a img for a course

---
+++ courses.js
@@ -0,0 +1,147 @@
+// Create the courses
+
+const courses = [
+
+    {
+        id: 1,
+        title: "blockchain dev course",
+        courseNumber: 1,
+        accesability: 'distance',
+        Course_start: '3 months',
+        info: 'Classroom or distance',
+        image_src: [
+            {
+                alt: 'A spiraling blockchain item that symbolizes tokens such as eth, btc and much more',
+                src: "assets\images\Blockchain.jpg"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Web Development Bootcamp",
+        courseNumber: 2,
+        accessibility: 'On-site',
+        courseStart: '2 months',
+        info: 'Classroom',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Data Science Fundamentals",
+        courseNumber: 3,
+        accessibility: 'Distance',
+        courseStart: '4 months',
+        info: 'Online',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Graphic Design Workshop",
+        courseNumber: 4,
+        accessibility: 'On-site',
+        courseStart: '1 month',
+        info: 'Classroom',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Mobile App Development",
+        courseNumber: 5,
+        accessibility: 'Distance',
+        courseStart: '3 months',
+        info: 'Online',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Photography Basics",
+        courseNumber: 6,
+        accessibility: 'On-site',
+        courseStart: '1.5 months',
+        info: 'Classroom',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 7,
+        title: "Business Management Essentials",
+        courseNumber: 7,
+        accessibility: 'Distance',
+        courseStart: '2.5 months',
+        info: 'Online',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 8,
+        title: "Artificial Intelligence in Business",
+        courseNumber: 8,
+        accessibility: 'On-site',
+        courseStart: '3 months',
+        info: 'Classroom',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 9,
+        title: "Digital Marketing Strategies",
+        courseNumber: 9,
+        accessibility: 'Distance',
+        courseStart: '2 months',
+        info: 'Online',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    },
+    {
+        id: 10,
+        title: "UX/UI Design Principles",
+        courseNumber: 10,
+        accessibility: 'On-site',
+        courseStart: '2 months',
+        info: 'Classroom',
+        image_src: [
+            {
+                alt: '',
+                src: ''
+            }
+        ]
+    }
+
+]
+console.log(courses);
